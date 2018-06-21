4d9f28a2881c03cafa6014ca36f4b7393623150d
esteban1234 <tva_libra@hotmail.com>
2018-06-21 10:56:46 -0500

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="qKO43e7S7wPZNjDYmjCIl_HFJbtLTpMLFs9LiF0c-8g" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>COMERCIALIZADORA DE PRODUCTOS ELECTRICOS HOUS SA DE CV</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
